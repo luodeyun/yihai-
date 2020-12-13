8a3cc95288c779da85d0082b056d0d2522b6d05e
ldy <807799686@qq.com>
2020-12-13 23:48:55 +0800

    yihai07city

--- src/components/electioncity/electioncity.vue
+++ src/components/electioncity/electioncity.vue
@@ -1,47 +1,63 @@
 <template>
-    <div id = 'app' >
-       <div class='iconHeader'>
-            <van-icon name="arrow-left" class='arrow-left' @click='goback' /> 
-            <van-search  v-model="searchvalue" placeholder="搜索城市或景点"     class="icon"  background="#bfa" />
-        </div>
-       
- <van-index-bar :index-list="indexList" class='Main'>
-  <van-index-anchor index="1">标题1</van-index-anchor>
-  <van-cell title="文本" />
-  <van-cell title="文本" />
-  <van-cell title="文本" />
-  <van-index-anchor index="2">标题2</van-index-anchor>
-  <van-cell title="文本" />
-  <van-cell title="文本" />
-  <van-cell title="文本" />
-  <van-index-anchor index="3">标题3</van-index-anchor>
-  <van-cell title="文本" />
-  <van-cell title="文本" />
-  <van-cell title="文本" />
+    <div class = 'content' >
+   
+     <!-- <van-sticky>
+         <van-nav-bar title='选择城市'
+         left-arrow>
+            <van-search  placeholder="请输入搜索关键词"></van-search>
+         </van-nav-bar>
+     </van-sticky> -->
+      <van-index-bar  @index-bar-sidebar-z-index='999'  >
+          <van-index-anchor index="A">标题1</van-index-anchor>
+               <van-cell title="文本" />
+               <van-cell title="文本" />
+               <van-cell title="文本" />
 
-  <van-index-anchor index="4">标题4</van-index-anchor>
-  <van-cell title="文本" />
-  <van-cell title="文本" />
-  <van-cell title="文本" />
-  <van-index-anchor index="5">标题5</van-index-anchor>
-  <van-cell title="文本" />
-  <van-cell title="文本" />
-  <van-cell title="文本" />
+           <van-index-anchor index="B">标题2</van-index-anchor>
+                <van-cell title="文本" />
+                <van-cell title="文本" />
+                <van-cell title="文本" />
+            <van-index-anchor index="C">标题c</van-index-anchor>
+                <van-cell title="文本" />
+                <van-cell title="文本" />
+                <van-cell title="文本" />
+            <van-index-anchor index="D">标题d</van-index-anchor>
+                <van-cell title="文本" />
+                <van-cell title="文本" />
+                <van-cell title="文本" />
 
-  <van-index-anchor index="6">标题2</van-index-anchor>
-  <van-cell title="文本" />
-  <van-cell title="文本" />
-  <van-cell title="文本" />
-  <van-index-anchor index="7">标题1</van-index-anchor>
-  <van-cell title="文本" />
-  <van-cell title="文本" />
-  <van-cell title="文本" />
+            <van-index-anchor index="E">标题e</van-index-anchor>
+                <van-cell title="文本" />
+                <van-cell title="文本" />
+                <van-cell title="文本" />
+             <van-index-anchor index="F">标题f</van-index-anchor>
+               <van-cell title="文本" />
+               <van-cell title="文本" />
+               <van-cell title="文本" />
 
-  <van-index-anchor index="8">标题2</van-index-anchor>
-  <van-cell title="文本" />
-  <van-cell title="文本" />
-  <van-cell title="文本" />
-</van-index-bar>
+           <van-index-anchor index="G">标题g</van-index-anchor>
+                <van-cell title="文本" />
+                <van-cell title="文本" />
+                <van-cell title="文本" />
+            <van-index-anchor index="H">标题h</van-index-anchor>
+                <van-cell title="文本" />
+                <van-cell title="文本" />
+                <van-cell title="文本" />
+            <van-index-anchor index="I">标i</van-index-anchor>
+                <van-cell title="文本" />
+                <van-cell title="文本" />
+                <van-cell title="文本" />
+
+            <van-index-anchor index="J">标题j</van-index-anchor>
+                <van-cell title="文本" />
+                <van-cell title="文本" />
+                <van-cell title="文本" />
+        </van-index-bar>
+
+
+
+      
+     
      </div>
 </template>;
 
@@ -50,45 +66,18 @@ export default {
   components: {},
   data() {
     return {
-      indexList: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,1, 2, 3, 4, 5, 6, 7, 8, 9, 10,1, 2, 3, 4, 5, 6, 7, 8, 9, 10,1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
       searchvalue: ""
     };
   },
   methods: {
-      goback(){
-          this.$router.go(-1)
-      }
+    goback() {
+      this.$router.go(-1);
+    }
   }
 };
 </script>
- <style scoped>
- .icon{
- width:320px;
-padding: 0;
-margin-right: 5px
- }
- .arrow-left{
-     width:30px;
-     height: 30px;
-     float: left;
-     text-align: center;
-     line-height: 30px
- }
- .iconHeader{
-     position: fixed ;
-     top:0;
-     left:0;
-     width:100%;
-     height: 40px;
-     position: fixed;
-     top:0px;
-     left:0;
-     z-index:999;
-     padding-top:5px;
-     background-color: #fff;
- }
-.Main{
-    margin-top:40px; 
-    
+ <style scoped lang='less'>
+.content{
+background: #eee;
 }
 </style>

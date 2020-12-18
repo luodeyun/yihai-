60d7b97e47b6d20bc5c8b23bd3b22548be22c94e
ldy <807799686@qq.com>
2020-12-18 16:16:21 +0800

    YiHai11

--- src/views/CustomerService.vue
+++ src/views/CustomerService.vue
@@ -1,22 +1,313 @@
 <template>
-    <div id = 'app'>
-        客服
-
-     </div>
+  <div id="customer">
+    <div class="nav-bar">
+      <span>客服中心</span>
+      <span><img src="@/assets/img/phone2.png" alt=""></span>
+    </div>
+    <div class="iconbar">
+      <span><img src="@/assets/img/kp.png" alt=""><p>开票管理</p></span>
+      <span><img src="@/assets/img/weizhang2.png" alt=""><p>违章处理</p></span>
+      <span><img src="@/assets/img/yijianfankui2.png" alt=""><p>意见反馈</p></span>
+    </div>
+    <div class="grass">
+      <div class="grass-top">
+        猜你想问</div>
+      <div class="grass-con">
+        <div class="grass-con-top">
+          <span>预定流程</span>
+          <span>免押金租车服务</span>
+        </div>
+        <div class="grass-con-bottom">
+          <span>预定流程</span>
+          <span>免押金租车服务</span>
+        </div>
+        <div class="grass-bottom">不是你想问的?联系人工客服</div>
+      </div>
+    </div>
+    <div class="bottom">
+      <div class="bottom-bar">
+        <span @click="onchange" :class="actionR ? 'demo' : ''">国内自驾</span>
+        <span @click="offchange" :class="actionG ? 'demo' : ''"> 高端专车</span>
+      </div>
+      <div class="roadtrip" v-if="actionR">
+        <van-collapse v-model="active">
+          <van-collapse-item title="基本租车规则" name="1" class="p">
+            <p>预定流程</p>
+            <p>修改订单</p>
+            <p>取车流程</p>
+            <p>车辆使用</p>
+            <p>车辆归还</p>
+            <p>服务完成</p>
+            <p>取消预定</p>
+          </van-collapse-item>
+          <van-collapse-item title="预付订单" name="2" class="p">
+            <p>预付订单规则</p>
+          </van-collapse-item>
+          <van-collapse-item title="注册登录" name="3" class="p">
+            <p>会员注册</p>
+            <p>找回密码</p>
+            <p>修改密码</p>
+            <p>注销账号</p>
+          </van-collapse-item>
+          <van-collapse-item title="事故处理" name="4" class="p">
+            <p>事故处理</p>
+            <p>车辆事故处理指引</p>
+            <p>基本保障服务费说明</p>
+            <p>增值服务</p>
+            <p>备用车使用</p>
+          </van-collapse-item>
+          <van-collapse-item title="芝麻信用" name="5" class="p">
+            <p>免押金租车服务？</p>
+            <p>详细规则？</p>
+            <p>什么是芝麻信用分？</p>
+            <p>如何查询芝麻信用分？</p>
+            <p>订单是否可以部分免押？</p>
+            <p>官网订单是否可以享受免押服务？</p>
+            <p>常用租车人是否可以享受免押服务？</p>
+            <p>订单费用预期是否会对芝麻信用造成影响</p>
+            <p>什么是负面记录</p>
+            <p>双免订单修改</p>
+          </van-collapse-item>
+          <van-collapse-item title="友情提示" name="6" class="p">
+            <p>客服热线</p>
+            <p>投诉</p>
+            <p>避免高峰取车还车</p>
+            <p>新用户</p>
+            <p>客户信息变更</p>
+          </van-collapse-item>
+          <van-collapse-item title="违章处理" name="7" class="p">
+            <p>处理违章的材料</p>
+            <p>违章处理地</p>
+            <p>行驶证扫描件</p>
+            <p>预约原件</p>
+            <p>违章处理中</p>
+            <p>违章处理后</p>
+          </van-collapse-item>
+          <van-collapse-item title="积分商城" name="8" class="p">
+            <p>积分比例</p>
+            <p>积分获得</p>
+            <p>积分规则</p>
+            <p>积分查询</p>
+            <p>积分兑换</p>
+            <p>积分抵扣</p>
+            <p>积分有效期</p>
+          </van-collapse-item>
+        </van-collapse>
+      </div>
+      <div class="gaoduan" v-if="actionG">
+        <van-collapse v-model="activeName" accordion>
+          <van-collapse-item title="查询信息" name="1" class="p">
+            <p>用车城市</p>
+            <p>用车类型</p>
+            <p>提供车型</p>
+            <p>司机信息</p>
+            <p>查询订单</p>
+          </van-collapse-item>
+          <van-collapse-item title="预定" name="2" class="p">
+            <p>预定渠道</p>
+            <p>如何预定？</p>
+          </van-collapse-item>
+          <van-collapse-item title="账户相关" name="3" class="p">
+            <p>如何绑定信用卡</p>
+            <p>如何绑定企业账户</p>
+          </van-collapse-item>
+          <van-collapse-item title="取消" name="4" class="p">
+            <p>常规取消条款</p>
+            <p>紧急用车取消条款</p>
+          </van-collapse-item>
+          <van-collapse-item title="发票相关" name="5" class="p">
+            <p>如何申请发票</p>
+            <p>发票开具时效</p>
+            <p>没有收到电子发票</p>
+          </van-collapse-item>
+          <van-collapse-item title="费用相关" name="6" class="p">
+            <p>支付方式</p>
+          </van-collapse-item>
+          <van-collapse-item title="计费规则" name="7" class="p">
+            <p>超公里费</p>
+            <p>超时费</p>
+            <p>路桥费,停车费</p>
+            <p>驾驶员食宿费</p>
+            <p>空驶费</p>
+            <p>夜间服务费</p>
+            <p>等待费</p>
+          </van-collapse-item>
+        </van-collapse>
+      </div>
+    </div>
+  </div>
 </template>;
 
 <script>
-    export default{ 
-          components:{
-
-          },
-        data () {
-            return {
-                active:3
-            }
-        }
-     }
- </script>
- <style>
-
+export default {
+  components: {},
+  data() {
+    return {
+      activeName: ["0"],
+      actionR: true,
+      actionG: false,
+      active: ["0"],
+    };
+  },
+  methods: {
+    onchange() {
+      // 点击国家出现 高端消失
+      this.actionR = true;
+      this.actionG = false;
+    },
+    offchange() {
+      // 点击国家出现 高端消失
+      this.actionR = false;
+      this.actionG = true;
+    },
+  },
+};
+</script>
+ <style scoped lang='less'>
+#customer {
+  width: 100vw;
+}
+.nav-bar {
+  position: fixed;
+  padding: 2vw;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 96vw;
+  height: 4vh;
+  font-weight: 700;
+ background-color: #fff;
+  img{
+    width: 6vw;
+    height: 3vh;
+  }
+}
+.iconbar {
+  margin-top: 6vh;
+  width: 100vw;
+  height: 14vh;
+  // background-color: aqua;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  span {
+    width: 14vw;
+    height: 10vh;
+    // background-color: blanchedalmond;
+    display: flex;
+    justify-content: space-around;
+    flex-flow: column;
+    font-size: 12px;
+    img{
+      margin-left:2vw; 
+      width:10vw;
+      height: 6vh;
+    }
+   p{
+     margin-left:1vw;
+   }
+  }
+}
+.grass {
+  width: 100vw;
+  height: 29vh;
+  // background-color: chartreuse;
+  .grass-top {
+    width: 96vw;
+    height: 5vh;
+    padding:0 2vw;
+    // background-color: cornflowerblue;
+    font-weight: 700;
+  }
+  .grass-con {
+    width: 94vw;
+    margin: 0 3vw;
+    height: 16vh;
+    background-color: #bfa;
+    .grass-con-top {
+      width: 94vw;
+      height: 8vh;
+      background-color: #fff;
+      span {
+        display: inline-block;
+        width: 43vw;
+        height: 6vh;
+        border-radius: 10px;
+        background-color:rgb(224, 223, 223);
+        margin: 1vh 2vw;
+        text-align: center;
+        line-height: 6vh;
+        font-size:14px; 
+      }
+    }
+    .grass-con-bottom {
+      width: 94vw;
+      height: 8vh;
+      background-color: #fff;
+      span {
+        display: inline-block;
+        width: 43vw;
+        height: 6vh;
+        border-radius: 10px;
+        background-color: rgb(224, 223, 223);
+        margin: 1vh 2vw;
+        font-size:14px; 
+        text-align: center;
+        line-height: 6vh;
+      }
+    }
+  }
+  .grass-bottom {
+    width: 90vw;
+    height: 5vh;
+    margin: 1.5vw auto;
+    color: #FF9938;
+    border:1px solid orange; 
+    font-size: 14px;
+    letter-spacing: 2px;
+    background-color: #fff;
+    border-radius: 15px;
+    text-align: center;
+    line-height: 5vh;
+  }
+}
+.bottom {
+  width: 100vw;
+}
+.bottom-bar {
+  width: 50vw;
+  height: 5vh;
+  background-color: #fff;
+  span {
+    width: 23vw;
+    height: 5vh;
+    margin: 0 2vw;
+    text-align: center;
+    line-height: 5vh;
+    font-weight: 700;
+  }
+}
+.demo {
+  color: orange;
+}
+.roadtrip {
+  width: 94vw;
+  margin-top: 2vh;
+}
+.gaoduan {
+  width: 94vw;
+  margin-top: 2vh;
+}
+.p p {
+  width: 92vw;
+  height: 5vh;
+  //   text-align: center;
+  line-height: 5vh;
+  background-color: gainsboro;
+  border-bottom: 1px solid #dddddd;
+  margin: 2px 0;
+  padding: 0;
+}
 </style>
--- src/views/Mine.vue
+++ src/views/Mine.vue
@@ -1,42 +1,81 @@
 <template>
   <div id="mine">
-    <div class="mine-header">头部</div>
+    <div class="mine-header">
+      <span class="mine-header-img"> 头像 </span>
+      <div class="mine-header-rigth">
+        <span class="mine-header-right-phone">18372028861</span>
+        <div class="mine-header-right-vip">
+          <span>普通会员</span><span>待认证</span>
+        </div>
+      </div>
+    </div>
     <div class="mine-contain">
       <div class="mine-contain-first">我的订单</div>
       <div class="mine-contain-sconed">
-        <span class="mine-contain-sconed-c"><img src="@/assets/img/qiche.png" alt="">国内自驾</span>
-        <span class="mine-contain-sconed-c"><img src="@/assets/img/taocan.png" alt="">超值套餐</span>
-        <span class="mine-contain-sconed-c"><img src="@/assets/img/zhuanche.png" alt="">高端专车</span>
-        <span class="mine-contain-sconed-c"><img src="@/assets/img/jingdian.png" alt="">景点门票</span>
-       <span class="mine-contain-sconed-c"><img src="@/assets/img/haiwai.png" alt="">海外自驾</span>
+        <span class="mine-contain-sconed-c"
+          ><img src="@/assets/img/qiche.png" alt="" />国内自驾</span
+        >
+        <span class="mine-contain-sconed-c"
+          ><img src="@/assets/img/taocan.png" alt="" />超值套餐</span
+        >
+        <span class="mine-contain-sconed-c"
+          ><img src="@/assets/img/zhuanche.png" alt="" />高端专车</span
+        >
+        <span class="mine-contain-sconed-c"
+          ><img src="@/assets/img/jingdian.png" alt="" />景点门票</span
+        >
+        <span class="mine-contain-sconed-c"
+          ><img src="@/assets/img/haiwai.png" alt="" />海外自驾</span
+        >
       </div>
     </div>
-        <div class="mine-contain">
+    <div class="mine-contain">
       <div class="mine-contain-first">我的钱包</div>
       <div class="mine-contain-sconed">
-        <span class="mine-contain-sconed-c"><img src="@/assets/img/qianbao.png" alt="">国内自驾</span>
-       <span class="mine-contain-sconed-c"><img src="@/assets/img/jifen.png" alt="">国内自驾</span>
-       <span class="mine-contain-sconed-c"><img src="@/assets/img/youhuijuan.png" alt="">国内自驾</span>
-      <span class="mine-contain-sconed-c"><img src="@/assets/img/xyk.png" alt="">国内自驾</span>
-       <span class="mine-contain-sconed-c"><img src="@/assets/img/daijing.png" alt="">国内自驾</span>
+        <span class="mine-contain-sconed-c"
+          ><img src="@/assets/img/qianbao.png" alt="" />国内自驾</span
+        >
+        <span class="mine-contain-sconed-c"
+          ><img src="@/assets/img/jifen.png" alt="" />国内自驾</span
+        >
+        <span class="mine-contain-sconed-c"
+          ><img src="@/assets/img/youhuijuan.png" alt="" />国内自驾</span
+        >
+        <span class="mine-contain-sconed-c"
+          ><img src="@/assets/img/xyk.png" alt="" />国内自驾</span
+        >
+        <span class="mine-contain-sconed-c"
+          ><img src="@/assets/img/daijing.png" alt="" />国内自驾</span
+        >
       </div>
     </div>
-        <div class="mine-contain">
+    <div class="mine-contain">
       <div class="mine-contain-first">我的订单</div>
       <div class="mine-contain-sconed">
-        <span class="mine-contain-sconed-c"><img src="@/assets/img/qiye.png" alt="">企业信息</span>
-       <span class="mine-contain-sconed-c"><img src="@/assets/img/kaipiao.png" alt="">开票管理</span>
-       <span class="mine-contain-sconed-c"><img src="@/assets/img/weizhang.png" alt="">违章查询</span>
-      <span class="mine-contain-sconed-c"><img src="@/assets/img/help.png" alt="">帮助中心</span>
-       <span class="mine-contain-sconed-c"><img src="@/assets/img/weixing.png" alt="">公众号</span>
+        <span class="mine-contain-sconed-c"
+          ><img src="@/assets/img/qiye.png" alt="" />企业信息</span
+        >
+        <span class="mine-contain-sconed-c"
+          ><img src="@/assets/img/kaipiao.png" alt="" />开票管理</span
+        >
+        <span class="mine-contain-sconed-c"
+          ><img src="@/assets/img/weizhang.png" alt="" />违章查询</span
+        >
+        <span class="mine-contain-sconed-c"
+          ><img src="@/assets/img/help.png" alt="" />帮助中心</span
+        >
+        <span class="mine-contain-sconed-c"
+          ><img src="@/assets/img/weixing.png" alt="" />公众号</span
+        >
       </div>
     </div>
-    
- 
+
     <div class="mine-bottom">
       <div class="mine-bottom-first">
         <div>
-          <span class="mine-bottom-first-last"><img src="@/assets/img/liwu.png" alt=""></span>
+          <span class="mine-bottom-first-last"
+            ><img src="@/assets/img/liwu.png" alt=""
+          /></span>
           <span class="mine-bottom-first-last" style="width: 60px"
             >全民砍价</span
           >
@@ -45,7 +84,9 @@
       </div>
       <div class="mine-bottom-first">
         <div>
-          <span class="mine-bottom-first-last"><img src="@/assets/img/changyong.png" alt=""></span>
+          <span class="mine-bottom-first-last"
+            ><img src="@/assets/img/changyong.png" alt=""
+          /></span>
           <span class="mine-bottom-first-last" style="width: 60px"
             >常用信息</span
           >
@@ -54,7 +95,9 @@
       </div>
       <div class="mine-bottom-first">
         <div>
-          <span class="mine-bottom-first-last"><img src="@/assets/img/yijian.png" alt=""></span>
+          <span class="mine-bottom-first-last"
+            ><img src="@/assets/img/yijian.png" alt=""
+          /></span>
           <span class="mine-bottom-first-last" style="width: 60px"
             >意见反馈</span
           >
@@ -63,7 +106,9 @@
       </div>
       <div class="mine-bottom-first">
         <div>
-          <span class="mine-bottom-first-last"><img src="@/assets/img/fengxiang.png" alt=""></span>
+          <span class="mine-bottom-first-last"
+            ><img src="@/assets/img/fengxiang.png" alt=""
+          /></span>
           <span class="mine-bottom-first-last" style="width: 60px"
             >体验分享</span
           >
@@ -82,34 +127,70 @@ export default {
 };
 </script>
  <style lang='less' scoped>
-#mine {color: #666A6D;
+#mine {
+  color: #666a6d;
   padding: 0;
   margin: 0;
   width: 100vw;
   margin-bottom: 10vh;
   overflow: scroll;
-background-color: #F3F3F3;
+  background-color: #f3f3f3;
 }
 .mine-header {
   width: 100vw;
-  height: 20vh;
+  height: 18vh;
   background-color: #00ffff;
-   
+  display: flex;
+  align-items: center;
+  .mine-header-img {
+    display: inline-block;
+    width: 10vh;
+    height: 10vh;
+    margin: 0 5vh;
+    border-radius: 5vh;
+    text-align: center;
+    line-height: 10vh;
+    background-color: #fff;
+  }
+  .mine-header-rigth {
+    background-color: #fff;
+    width: 50vw;
+    height: 10vh;
+    .mine-header-right-phone {
+      background-color: #bfa;
+      display: inline-block;
+      text-align: center;
+      line-height: 6vh;
+      height: 6vh;
+    }
+    .mine-header-right-vip {
+      background-color: green;
+      height: 4vh;
+      width: 38vw;
+      color: #fff;
+      font-size: 12px;
+      line-height: 4vh;
+      display: flex;
+      justify-content: space-between;
+      span {
+        margin: 0 2vw;
+      }
+    }
+  }
 }
 .mine-contain {
   width: 96vw;
   height: 18vh;
   border-radius: 8px;
-  background-color: #fff;  
+  background-color: #fff;
   margin: 1vh auto;
 }
 .mine-contain-first {
   font-size: 14px;
   line-height: 3vh;
   height: 3vh;
-  padding:1vh;
-  border-bottom: 1px solid  #AAAAAA;
-;
+  padding: 1vh;
+  border-bottom: 1px solid #aaaaaa;
 }
 .mine-contain-sconed {
   display: flex;
@@ -123,17 +204,17 @@ background-color: #F3F3F3;
     background-color: #fff;
     font-size: 12px;
     text-align: center;
-    img{
+    img {
       display: block;
-      width:8vw;
+      width: 8vw;
       height: 4vh;
-      margin: 1vh auto
+      margin: 1vh auto;
     }
   }
 }
 .mine-bottom {
   width: 96vw;
-  margin:  auto;
+  margin: auto;
   padding: 0;
   border-radius: 8px;
   background-color: #fff;
@@ -142,23 +223,23 @@ background-color: #F3F3F3;
     width: 94vw;
     height: 8vh;
     margin-left: 2vw;
-    border-bottom: 1px solid #BCB9B9;
+    border-bottom: 1px solid #bcb9b9;
     display: flex;
     justify-content: space-between;
-    
+
     .mine-bottom-first-last {
       display: inline-block;
-      margin-top:1vh; 
-   
+      margin-top: 1vh;
+
       width: 6vw;
       height: 6vh;
       line-height: 6vh;
       text-align: center;
-      
-      img{
-      margin-bottom:-0.5vh; 
-        width:4vw;
-        height:2.5vh
+
+      img {
+        margin-bottom: -0.5vh;
+        width: 4vw;
+        height: 2.5vh;
       }
     }
   }
